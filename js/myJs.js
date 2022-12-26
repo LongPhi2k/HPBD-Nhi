f6d68d9fe579c3453e7e4b8938af17a861f94dbe
LongPhi2k <117796470+LongPhi2k@users.noreply.github.com>
2022-12-26 14:44:26 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://longphi2k.github.io/chuc-mung-sinh-nhat-Nhi/";
           },
         });
       }
